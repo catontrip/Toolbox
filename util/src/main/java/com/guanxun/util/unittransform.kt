72acb394f9c12b5c22088fc4483c5252ccf1a4fe
Tom <catontrip@live.com>
2024-12-24 22:09:35 +0800

    initial version

---
+++ util/src/main/java/com/guanxun/util/unittransform.kt
@@ -0,0 +1,28 @@
+package com.guanxun.util
+import android.util.TypedValue
+import android.content.res.Resources
+
+/**
+ * dp转px
+ *
+ * @param dpValue density-independent pixel
+ * @return 转换后的px值
+ */
+fun dp2px(dpValue: Float): Float {
+    return TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP, dpValue, Resources.getSystem().displayMetrics
+    )
+}
+
+/**
+ * sp转换px
+ *
+ * @param spValue scaled pixels
+ * sp值
+ * @return 转换后的px值
+ */
+fun sp2px(spValue: Float): Float {
+    return TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_SP, spValue, Resources.getSystem().displayMetrics
+    )
+}
